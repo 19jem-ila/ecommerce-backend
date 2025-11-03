80a33d900cb7eaa8f79cf8d8d9c9c8a75a4695fc
unknown <jemilareedwan@gmail.com>
2025-11-02 19:36:39 -0800

    cors modified

--- server.js
+++ server.js
@@ -31,14 +31,30 @@ const PORT = process.env.PORT || 5000;
 // Connect DB
 connectDB();
 
+const allowedOrigins = [
+ 
+  "http://localhost:3000",
+  "https://ecommerce-frontend-main-9.onrender.com"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+
+
 // Middleware
 app.use(helmet());
 app.use(compression());
 app.use(morgan('combined'));
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
-}));
+
 app.use(express.json());           // ✅ required to parse JSON
 app.use(express.urlencoded({ extended: true }));
 
@@ -63,22 +79,7 @@ app.get('/api/health', (req, res) => {
   });
 });
 
-const allowedOrigins = [
- 
-  "http://localhost:3000",
-  "https://ecommerce-frontend-main-9.onrender.com"
-];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
 
 
 // Error handlers
